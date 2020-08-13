768309930f40c7d2aa815a6aa02110bd0edaf406
Subikesh <subikeshps@gmail.com>
2020-08-13 17:42:00 +0530

    Functionality for each pop-up card

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 // Function to pull up and push back the card for resume
 document.addEventListener("click", function(event) {
-    var card = document.getElementById("resume-card");
-    var title = document.querySelector(".card-title");
-    if (card.contains(event.target)) {
-        card.classList.add("active");
-        title.classList.add("display-3");
-    }
-    else {
-        card.classList.remove("active");
-        title.classList.remove("display-3");
+    var cards = ["resume", "about", "project", "contact"]
+    for (let i = 0; i < cards.length; i++) {
+        console.log(cards[i]+"-card")
+        var card = document.getElementById(cards[i]+"-card");
+        var title = document.querySelector("#"+cards[i]+"-card .card-title");
+        if (card.contains(event.target)) {
+            card.classList.add("active");
+            title.classList.add("display-3");
+        }
+        else {
+            card.classList.remove("active");
+            title.classList.remove("display-3");
+        }
+        
     }
 });
